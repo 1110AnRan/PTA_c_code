4e2b722d16d0c9a836a9a289bc94f9ef740df825
安然 <2791158256@qq.com>
2022-11-27 12:59:07 +0800

    PTA作业第六周

---
+++ test.10.4/test.10.4/PTA作业第六周.cpp
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int n, b;
+//	scanf_s("%d %d", &n, &b);
+//	if (n % b == 0)
+//	{
+//		printf("%d", n / b);
+//	}
+//	else
+//	{
+//		printf("%d", (n / b) + 1);
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int N;
+//	scanf_s("%d", &N);
+//	if ((N % 5) <= 3)
+//	{
+//		printf("Fishing in day %d", N);
+//	}
+//	else if ((N % 5) > 3)
+//	{
+//		printf("Drying in day %d", N);
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//    int n = 0;
+//    int m = 0;
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;;
+//    scanf_s("%d", &n);
+//    for (m = 1; m <= n; m++)
+//    {
+//        scanf_s("%d", &a);
+//    }
+//    for (m = 1; m <= n; m++)
+//    {
+//        if (a % 2 == 1)
+//        {
+//            b++;
+//        }
+//        else if()
+//        {
+//            c++;
+//        }
+//    }
+//    printf("%d %d",b, c);
+//    return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int N = 0;
+//	int M = 0;
+//	double	ret = 1;
+//	double	 a = 0;
+//	double sum = 0;
+//	scanf("%d", &N);
+//	for (M = 1; M <= N; M++)
+//	{
+//		a = a + (1/ret);
+//		ret++;
+//	}
+//	sum = a;
+//	printf("sum = %.6lf", sum);
+//	return 0;
+//
+//}
+//#include <stdio.h>
+//int main()
+//{
+//    int N;
+//    int m = 0;
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    scanf("%d", &N);
+//    for (m = 0; m < N; m++)
+//    {
+//        scanf("%d", &a);
+//        if (a % 2 == 1)
+//        {
+//            b++;
+//        }
+//        else if (a % 2 == 0)
+//        {
+//            c++;
+//        }
+//    }
+//    printf("%d %d", b, c);
+//    return 0;
+//}
